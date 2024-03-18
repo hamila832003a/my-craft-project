24557da1f86a2ed54c4518cd6a5ad14390ad653a
hamila832003a <hamila832003@gmail.com>
2024-03-18 11:10:40 -0500

    update prior to update to craft 5

--- web/javascript/index.js
+++ web/javascript/index.js
@@ -23,11 +23,48 @@ const timeline = gsap.timeline({
 ;
 
 
-
-gsap.fromTo(".circleRing", 
+  const anim = gsap.fromTo(".circleRing", 
   {rotation: 0},
   {rotation: 360,
    repeat: -1,
    ease: "none",
    duration: 6
   });
+
+  ScrollTrigger.create({
+    trigger: ".c",
+    animation: anim,
+    // Uncomment these to see how they affect the ScrollTrigger
+    // markers: true,
+    // start: "top center",
+    // end: "top 100px",
+    // toggleClass: "active",
+    // pin: true,
+    // scrub: 1,
+    // onUpdate: self => {
+    //   console.log("progress:", self.progress.toFixed(3), "direction:", self.direction, "velocity", self.getVelocity());
+    // }
+  });
+
+  "use strict";
+gsap.registerPlugin(ScrollTrigger);
+
+/* this needs to be fixed on resize */
+gsap.utils.toArray(".slider").forEach(element => {
+
+  let sliderWidth = element.offsetWidth;
+
+  gsap.to(element, {
+    x: `-${document.querySelector(".slider").clientWidth - window.innerWidth}px`,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".section-scroll",
+      start: "top top",
+      end: "+=" + sliderWidth,
+      pin: true,
+      // pinSpacing: true,
+      scrub: 1,
+      markers: true,
+    },
+  });
+});
